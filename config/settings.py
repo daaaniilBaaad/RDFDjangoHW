ca83c767639f897d43875e20ef972e7a64d86476
daaaniil <daaaniil1234@gmail.com>
2026-01-25 18:34:06 +0300

    HW35.2 vol13

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ else:
             "NAME": os.getenv("NAME"),
             "USER": os.getenv("USER"),
             "PASSWORD": os.getenv("PASSWORD"),
-            "HOST": os.getenv("HOST", "localhost"),
+            "HOST": os.getenv("HOST", "127.0.0.1"),
             "PORT": os.getenv("PORT", "5432"),
         }
     }
